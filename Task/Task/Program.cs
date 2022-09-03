0366cccede7586a6c32ac2e899758b60f12fd680
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-09-04 00:38:56 +0300

    Add a method to fill the array with random number

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -6,7 +6,19 @@ namespace Task
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _ = int.TryParse(Console.ReadLine(), out var size);
+
+            var arrayOfNumbers = GetArrayWithRandomElements(new int[size]);
+        }
+
+        public static int[] GetArrayWithRandomElements(int[] arrayOfNumbers)
+        {
+            for (int index = 0; index < arrayOfNumbers.Length; index++)
+            {
+                arrayOfNumbers[index] = new Random().Next(1, 27);
+            }
+
+            return arrayOfNumbers;
         }
     }
 }
