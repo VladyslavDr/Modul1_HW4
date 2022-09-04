86af2c5e9271fa0bc4247dbac29d8ce80cc1cf2c
Vladyslav Drushliak <vladislav.drushliak@gmail.com>
2022-09-04 11:13:00 +0300

    Add a method to divide the array into even and odd values

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -8,17 +8,81 @@ namespace Task
         {
             _ = int.TryParse(Console.ReadLine(), out var size);
 
-            var arrayOfNumbers = GetArrayWithRandomElements(new int[size]);
+            var array = GetArrayWithRandomElements(new int[size]);
+
+            ShowArray(array);
+
+            int[] arrayOfEvenNumbers;
+            int[] arrayOfOddNumbers;
+
+            SplitAtOddAndEven(array, out arrayOfEvenNumbers, out arrayOfOddNumbers);
+
+            ShowArray(arrayOfEvenNumbers);
+            ShowArray(arrayOfOddNumbers);
         }
 
-        public static int[] GetArrayWithRandomElements(int[] arrayOfNumbers)
+        public static void SplitAtOddAndEven(int[] array, out int[] arrayOfEvenNumbers, out int[] arrayOfOddNumbers)
         {
-            for (int index = 0; index < arrayOfNumbers.Length; index++)
+            int numberEvenElements;
+            int numberOddElements;
+
+            GetNumberOfEvenAndOddElements(array, out numberEvenElements, out numberOddElements);
+
+            arrayOfEvenNumbers = new int[numberEvenElements];
+            arrayOfOddNumbers = new int[numberOddElements];
+
+            int index1 = 0;
+            int index2 = 0;
+
+            foreach (var number in array)
             {
-                arrayOfNumbers[index] = new Random().Next(1, 27);
+                if (number % 2 == 0)
+                {
+                    arrayOfEvenNumbers[index1++] = number;
+                }
+                else
+                {
+                    arrayOfOddNumbers[index2++] = number;
+                }
+            }
+        }
+
+        public static void ShowArray(int[] array)
+        {
+            foreach (var number in array)
+            {
+                Console.Write($"{number} ");
             }
 
-            return arrayOfNumbers;
+            Console.WriteLine();
+        }
+
+        public static void GetNumberOfEvenAndOddElements(int[] array, out int even, out int odd)
+        {
+            even = 0;
+            odd = 0;
+
+            foreach (var number in array)
+            {
+                if (number % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+            }
+        }
+
+        public static int[] GetArrayWithRandomElements(int[] array)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                array[index] = new Random().Next(1, 27);
+            }
+
+            return array;
         }
     }
 }
